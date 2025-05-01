4a412c1b572eb69d8c05c85f0f24b1ed5a4b1dbb
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2025-05-01 21:54:36 +0700

    # feat createtelemetry json stringify function

--- include/telemetry.h
+++ include/telemetry.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <string>
+#include <nlohmann/json.hpp>
+
+using std::string;
+using JSON = nlohmann::json;
+
 struct Telemetry {
+  string stringify() {
+    JSON json;
+
+    json["nitrogen"] = this->nitrogen;
+    json["phosphorus"] = this->phosphorus;
+    json["kalium"] = this->kalium;
+    json["pH"] = this->pH;
+    json["moisture"] = this->moisture;
+    json["light"] = this->light;
+    json["temperature"] = this->temperature;
+    
+    return json.dump();
+  };
+
   float nitrogen;
   float phosphorus;
   float kalium;
